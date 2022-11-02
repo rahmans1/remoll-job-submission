5f77f0e74ecd0a79ff7ba00ff6ac706833a6672f
Sakib Rahman <rahmans@myumanitoba.ca>
2022-11-02 14:01:12 -0500

    Adding placeholder bin sizes and min max ranges for variables

--- analysis/rate_col4bore_track/analyse.C
+++ analysis/rate_col4bore_track/analyse.C
@@ -24,18 +24,36 @@ int analyse(TString source, TString out, TString gen, TString detid, TString wri
   ctype.push_back("col4bore");
   ctype.push_back("col4boreandacceptance");
   
-  std::map<TString, Int_t> nbinx_xy;
-  std::map<TString, Int_t> nbiny_xy;
-  std::map<TString, Double_t> minx_xy;
-  std::map<TString, Double_t> maxx_xy;
-  std::map<TString, Double_t> miny_xy;
-  std::map<TString, Double_t> maxy_xy;
-  std::map<TString, Int_t> nbin_r;
-  std::map<TString, Double_t> min_r;
-  std::map<TString, Double_t> max_r;
-  std::map<TString, Int_t> nbin_e; 
-  std::map<TString, Double_t> min_e;
-  std::map<TString, Double_t> max_e;
+  std::map<TString, Int_t> nbin{ {"MD_nocut", 1500}, \
+                                 {"MD_nocut", 1500}, \
+                                 {"Col4Ent", 100}, \
+                                 {"Col4Exit", 100}, \
+                                 {"Col6AEnt", 140}, \
+                                 {"Col6AMid", 140}, \
+                                 {"Col6AExit", 140}, \
+                                 {"Col6BEnt", 140}, \
+                                 {"Col6BMid", 140}, \
+                                 {"Col6BExit", 140} };
+  std::map<TString, Double_t> min{ {"MD", -1500}, \
+                                       {"Col4Ent", -500}, \
+                                       {"Col4Exit", -500}, \
+                                       {"Col6AEnt", -700}, \
+                                       {"Col6AMid", -700}, \
+                                       {"Col6AExit", -700}, \
+                                       {"Col6BEnt", -700}, \
+                                       {"Col6BMid", -700}, \
+                                       {"Col6BExit", -700} };
+  std::map<TString, Double_t> max{ {"MD", 1500}, \
+                                       {"Col4Ent", 500}, \
+                                       {"Col4Exit", 500}, \
+                                       {"Col6AEnt", 700}, \
+                                       {"Col6AMid", 700}, \
+                                       {"Col6AExit", 700}, \
+                                       {"Col6BEnt", 700}, \
+                                       {"Col6BMid", 700}, \
+                                       {"Col6BExit", 700} };
+  
+ 
   
   
   std::map<TString, TH2D*> h_xy;
@@ -44,13 +62,25 @@ int analyse(TString source, TString out, TString gen, TString detid, TString wri
 
   for(Int_t i=0; i<ptype.size(); i++){
    for(Int_t j=0; j<ctype.size(); j++){
-   part= Form("pr_%s_%s_%s_%s_%s", detector.Data(), sector[j].Data(), pid.Data(), photon_ene[k].Data(), photon_mom[l].Data());
+   part= Form("%s_%s_%s", detector.Data(), ptype.Data(), ctype.data());
    h_xy[part]=new TH1D(part+"_xy", Form("%s_xy rate-weighted distro, Generator=%s", part.Data(), gen.Data()), , -500, 500, 100, -500, 500);
    h_r[part]=new TH2D(part+"_r", Form("%s_r rate-weighted distro, Generator=%s", part.Data(), gen.Data()), 300, 0, 1500);
    h_e[part]=new TH2D(part+"_e", Form("%s_prpz rate-weighted distribution, Generator=%s", part.Data(), gen.Data()), 100, 0, 11000);
    }  
   }
 
+  
+  
+  
+  std::map<TString, Int_t> detector_cut{ {"MD", hit.det==28}, \
+                                         {"Col4Ent", hit.det==44}, \
+                                         {"Col4Exit", hit.det==45}, \
+                                         {"Col6AEnt", hit.det==66}, \
+                                         {"Col6AMid", hit.det==68}, \
+                                         {"Col6AExit", hit.det==69}, \
+                                         {"Col6BEnt", hit.det==72}, \
+                                         {"Col6BMid", hit.det==73}, \
+                                         {"Col6BExit", hit.det==74} };
   return 0; 
 }
 
